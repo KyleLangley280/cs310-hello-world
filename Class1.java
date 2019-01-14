cd68c832f443361c65bb5001f01d0d461fb26455
Kyle Langley <klangley3@stu.jsu.edu>
2019-01-14 12:09:27 -0600

    CS310

--- Class1.java
+++ Class1.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author Kyle Langley
  */
 
 public class Class1 {
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author Kyle Langley
  */
 
 public class HelloWorld {
